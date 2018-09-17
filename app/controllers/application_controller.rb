c7529f88b5428172dcf0fea0e20d3f4a0acf5455
Madeline Stark <madeline.stark1@gmail.com>
2018-09-17 23:01:46 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,12 +27,17 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/posts/:id' do
-    @post = Post.find_by(id: params[:id]) 
+    @post = Post.find_by(id: params[:id])
     erb :show
   end
 
   get '/posts/:id/edit' do #id is being interpolated here-colon makes dynamic route
     erb :edit
+  end
+
+  post '/posts/:id/edit' do
+    @post.content = params
+    erb :show
   end
 
   delete '/posts/:id/delete' do
